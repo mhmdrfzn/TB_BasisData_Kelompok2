bd36ab431ef858858b1771eccd394575b363eef1
mhmdrfzn <fauzan.risyad@gmail.com>
2025-06-15 17:17:11 +0700

    create.php

--- ukm_keuangan/users/create.php
+++ ukm_keuangan/users/create.php
@@ -4,7 +4,7 @@ include '../db.php';
 
 if ($_SERVER["REQUEST_METHOD"] == "POST") {
     $username = $_POST["username"];
-    $password = password_hash($_POST["password"], PASSWORD_DEFAULT);
+    $password = $_POST["password"];
     $role = $_POST["role"];
 
     $stmt = $conn->prepare("INSERT INTO users (username, password, role) VALUES (?, ?, ?)");
